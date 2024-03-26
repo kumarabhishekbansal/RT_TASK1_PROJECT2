a675c40aaee8379095ce21a8d59b432feb1d30bf
kumarabhishekbansal <kumarabhishekbansal>
2024-03-26 21:34:06 +0530

    created pages folder and profile page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './assets/logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Profile from "./pages/Profile";
 function App() {
   return (
     <div className="App">
         <Header logo={logo} />
+        <Profile userName="kumarabhishekbansal" />
     </div>
   );
 }
