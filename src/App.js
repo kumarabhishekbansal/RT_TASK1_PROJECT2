796a616d494787c5301070d4fbc08a2ff8a33cd0
kumarabhishekbansal <kumarabhishekbansal>
2024-03-26 22:19:38 +0530

    created projects page with react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './assets/logo.svg';
-import './App.css';
-import Header from './components/Header';
+import logo from "./assets/logo.svg";
+import "./App.css";
+import Header from "./components/Header";
 import Profile from "./pages/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Projects from "./pages/Projects";
+import ProjectDetail from "./pages/ProjectDetail";
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
         <Header logo={logo} />
-        <Profile userName="kumarabhishekbansal" />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Profile userName={"kumarabhishekbansal"} />}
+          />
+
+          <Route
+            exact
+            path="/projects"
+            element={<Projects userName={"kumarabhishekbansal"} />}
+          />
+
+          <Route
+            path="/projects/:name"
+            element={<ProjectDetail userName="kumarabhishekbansal" />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/ProjectDetail.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+function ProjectDetail({ userName }) {
+  const [loading, setLoading] = useState(false);
+  const [project, setProject] = useState([]);
+  const { name } = useParams();
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch(
+        `https://api.github.com/repos/${userName}/${name}`
+      );
+      const result = await data.json();
+      if (result) {
+        console.log(result);
+        setProject(result);
+        setLoading(false);
+      }
+    }
+    if (userName && name) {
+      fetchData();
+    }
+  }, [userName, name]);
+  return (
+    <div className="Project-container">
+      <h2>Project: {project.name}</h2>
+      {loading ? <span>Loading...</span> : <div>
+        
+      </div>}
+    </div>
+  );
+}
+export default ProjectDetail;
